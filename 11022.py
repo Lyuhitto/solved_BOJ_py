847f6f112f9f875087b03a1f4fd147ba8d668010
Lyuhitto <liany0616@gmail.com>
2020-05-18 23:55:36 +0900

    Solve question 11022
    Solve question 11022.

---
+++ 11022.py
@@ -0,0 +1,10 @@
+# https://www.acmicpc.net/problem/11022
+
+import sys
+
+
+n = int(input())
+
+for i in range(n):
+  a, b = map(int, sys.stdin.readline().strip().split())
+  print("Case #%d: %d + %d = %d" % (i + 1, a, b, a + b))
