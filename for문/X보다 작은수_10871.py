6cb66f9e52dc452e42824263dcc9de267669bfac
Lyuhitto <liany0616@gmail.com>
2020-05-23 00:42:11 +0900

    Solve X보다 작은수_10871

---
+++ for문/X보다 작은수_10871.py
@@ -0,0 +1,14 @@
+import sys
+
+
+n, x = map(int, sys.stdin.readline().strip().split())
+number = map(int, sys.stdin.readline().strip().split())
+result = []
+
+for i in number:
+    if i < x:
+        assert type(i) is int, '정수 아님'
+        result.append(i)
+for i in result:
+    assert type(i) is int, '정수 아님'
+    print(i, end = ' ')
