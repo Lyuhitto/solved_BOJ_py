3c971a532b0f92b8f618e361f28cfe431c1c561b
Lyuhitto <liany0616@gmail.com>
2020-05-23 01:08:18 +0900

    Solve 최소, 최대_10818
    Solve.
    readline funtion is faster than input function.

---
+++ 1차원 배열/최소, 최대_10818.py
@@ -0,0 +1,8 @@
+import sys
+
+n = int(input())
+number_list = list(map(int, sys.stdin.readline().strip().split()))
+list_max = max(number_list)
+list_min = min(number_list)
+
+print("%d %d" %(list_min, list_max))
