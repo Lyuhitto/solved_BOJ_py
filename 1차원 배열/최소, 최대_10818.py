93948d3dfb0c277df5b29ad4ccc1230a4929e03a
Lyuhitto <liany0616@gmail.com>
2020-05-29 23:50:16 +0900

    Flake8, Update
    Not Solved, flake8

--- 1차원 배열/최소, 최대_10818.py
+++ 1차원 배열/최소, 최대_10818.py
@@ -5,4 +5,4 @@ number_list = list(map(int, sys.stdin.readline().strip().split()))
 list_max = max(number_list)
 list_min = min(number_list)
 
-print("%d %d" %(list_min, list_max))
+print("%d %d" % (list_min, list_max))
--- while문/더하기 사이클_1110.py
+++ while문/더하기 사이클_1110.py
@@ -1,14 +1,18 @@
-def cycle(number) :
-    x = number
+def cycle(number):
     result = 0
-    cycle_lenght = 0
-    while result == number :
-        units = x % 10
-        tens = x // 10
-        plus = units + tens
-        result = units * 10 + plus if plus < 10 else (plus - 10)
-        cycle_lenght += 1
-    return cycle_lenght
+    cycle_length = 0
+
+    while result != number:
+        tens = number // 10
+        print(tens)
+        units = number % 10
+        print(units)
+        plus = tens + units
+        print(plus)
+        result = units * 10 + (plus if plus < 10 else (plus - 10))
+        cycle_length += 1
+    return cycle_length
+
 
 x = int(input())
 print(cycle(x))
