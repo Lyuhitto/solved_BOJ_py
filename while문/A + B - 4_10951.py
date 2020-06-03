4654255333a42cf06636057cd520b10d61444a61
Lyuhitto <liany0616@gmail.com>
2020-06-04 00:25:55 +0900

    Inspect with flake8
    Inspect with flake8

--- while문/A + B - 4_10951.py
+++ while문/A + B - 4_10951.py
@@ -1,7 +1,8 @@
 import sys
 
-try:
-    while 1:
+while 1:
+    try:
         a, b = map(int, sys.stdin.readline().strip().split())
-        print(a + b)
-except:exit()
+    except EOFError:
+        break
+    print(a + b)
