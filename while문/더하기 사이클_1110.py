10d01fc0d91388b3b8dce8cf4a4acdf84d324871
Lyuhitto <liany0616@gmail.com>
2020-06-04 00:52:00 +0900

    Solve 더하기 사이클_1110
    Solve and inspect with flake8.

--- while문/더하기 사이클_1110.py
+++ while문/더하기 사이클_1110.py
@@ -1,18 +1,12 @@
-def cycle(number):
-    result = 0
-    cycle_length = 0
+n = int(input())
+a = n
+cycle_length = 0
 
-    while result != number:
-        tens = number // 10
-        print(tens)
-        units = number % 10
-        print(units)
-        plus = tens + units
-        print(plus)
-        result = units * 10 + (plus if plus < 10 else (plus - 10))
-        cycle_length += 1
-    return cycle_length
+while a != n or cycle_length == 0:
+    units = a % 10
+    ten = a // 10
+    value = (units + ten) % 10
+    a = units * 10 + value
+    cycle_length += 1
 
-
-x = int(input())
-print(cycle(x))
+print(cycle_length)
