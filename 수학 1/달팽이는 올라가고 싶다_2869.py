80cf70418e6cf7fc02f210689aee502d568a942e
Lyuhitto <liany0616@gmail.com>
2020-08-02 22:37:18 +0900

    Fix Condition to raise ValueError
    Fix the Condition to raise ValueError of def snail_can_go_up.

--- 수학 1/달팽이는 올라가고 싶다_2869.py
+++ 수학 1/달팽이는 올라가고 싶다_2869.py
@@ -1,5 +1,5 @@
 def snail_can_go_up(A, B, V):
-    if not(B < A or A <= V):
+    if not(B < A and A <= V):
         raise ValueError("These are not B<A and A<=V")
     day = (V-B)/(A-B)
     return int(day) if day == int(day) else int(day) + 1
