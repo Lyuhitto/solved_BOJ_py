577d9622871b2d406919227be9ff9cea8843a524
Lyuhitto <liany0616@gmail.com>
2020-08-13 00:47:52 +0900

    Improve range in for-loop
    Improve range in for-loop.

--- 브루트 포스/분해합_2231.py
+++ 브루트 포스/분해합_2231.py
@@ -1,6 +1,15 @@
-n = int(input())
-result = 0
-for i in range(10, 1000001):
-    pass
+import time
 
-print(result)
+
+def decomposition(n):
+    min_n = 10 if (n/2) < 10 else n - len(str(n)) * 9
+    for i in range(min_n, n):
+        result = i + sum([int(x) for x in str(i)])
+        if result == n:
+            return i
+    return 0
+
+
+start = time.time()
+print(decomposition(int(input())))
+print(f"time : {time.time()-start}")
