dc53168d01ee5725660e35f7a09d99aebc95e742
Lyuhitto <liany0616@gmail.com>
2020-09-18 23:40:30 +0900

    Solve 소수 구하기
    Solve

---
+++ 소수 구하기_1929.py
@@ -0,0 +1,15 @@
+def is_prime(num):
+    if num == 1:
+        return False
+    else:
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+
+
+m, n = map(int, input().split())
+
+for j in range(m, n+1):
+    if is_prime(j):
+        print(j)
