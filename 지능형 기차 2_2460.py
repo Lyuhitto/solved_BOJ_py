3b5a884c330fbda64572b963c7fb8c4ac65106d2
Lyuhitto <liany0616@gmail.com>
2021-02-23 00:54:18 +0900

    Solved

--- 지능형 기차 2_2460.py
+++ 지능형 기차 2_2460.py
@@ -1,7 +1,7 @@
 board = 0
 board_max = 0
 
-for _ in range(4):
+for _ in range(10):
     alighting, boarding = map(int, input().split())
     board += (boarding - alighting)
     board_max = board if board > board_max else board_max
